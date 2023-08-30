66ff12122f43587644e58fb9985653a5166c9959
TahaBinhuraib <taha.huraib@gmail.com>
2023-08-31 00:40:50 +0300

    add kaggle dataset

---
+++ ft_datasets/kaggle_dataset.py
@@ -0,0 +1,63 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+# For dataset details visit: https://crfm.stanford.edu/2023/03/13/alpaca.html
+
+import copy
+import json
+import os
+import torch
+import pandas as pd
+
+from sentencepiece import SentencePieceProcessor
+from torch.utils.data import Dataset
+from typing import List
+from .utils import filter_notebook
+
+
+class InstructionDataset(Dataset):
+    def __init__(self, dataset_config, tokenizer, partition="train", max_words=30):
+        if partition == "train":
+            self.ann = pd.read_json(dataset_config.train_split+"/train.json")
+        elif partition == "val":
+            self.ann = pd.read_json(dataset_config.test_split+"/val.json")
+
+        self.max_words = max_words
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.ann)
+
+    def __getitem__(self, index):
+        IGNORE_INDEX = -100  # The default setting in CrossEntropyLoss
+
+        ann = self.ann.iloc[index]
+        prompt = ann["Instruction"]
+        example = prompt + '\n' + ann["Notebook"]  # This is the full example
+        prompt = torch.tensor(
+            self.tokenizer.encode(prompt), dtype=torch.int64
+        )
+        example = self.tokenizer.encode(example)
+        example.append(self.tokenizer.eos_token_id)
+        example = torch.tensor(
+            example, dtype=torch.int64
+        )
+        padding = self.max_words - example.shape[0]
+        if padding > 0:
+            example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
+        elif padding < 0:
+            example = example[: self.max_words]
+        labels = copy.deepcopy(example)
+        labels[: len(prompt)] = -1
+        example_mask = example.ge(0)
+        label_mask = labels.ge(0)
+        example[~example_mask] = 0
+        labels[~label_mask] = IGNORE_INDEX
+        example_mask = example_mask.float()
+        label_mask = label_mask.float()
+
+        return {
+            "input_ids": example,
+            "labels": labels,
+            "attention_mask": example_mask,
+        }
